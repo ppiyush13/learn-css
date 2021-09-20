6521efa6305ab4923a9cdb67a912db68b212bf9a
Piyush Lodaya <ppiyush13@gmail.com>
2021-09-20 19:48:48 +0530

    css selectors and reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components/macro';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper>
+      <main className={'first-main'}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>Content 1</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </main>
+      <main>
+        <p>
+          <span>Content 2</span>
+        </p>
+      </main>
+      <main>
+        <p>
+          <span>Content 3</span>
+        </p>
+      </main>
+      <main>
+        <p>
+          <span>Content 4</span>
+        </p>
+      </main>
+    </Wrapper>
   );
-}
+};
 
-export default App;
+const Wrapper = styled.div`
+  main {
+    margin-top: 1rem;
+  }
+  main span {
+    display: inline-block;
+    width: 100%;
+    background-color: wheat;
+    padding: 0.25rem 1rem;
+  }
+`;
---
+++ src/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    html {
+        font-size: 16px;
+    }
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        border: none;
+    }
+`;
